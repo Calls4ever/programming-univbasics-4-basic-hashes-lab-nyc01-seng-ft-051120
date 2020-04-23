0d287d1d915a2bab023148b4487291e19939f3bf
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-23 22:24:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  Hash.new { ":Name"  "tsering", ":age" 45}
+new_hash= Hash.new{:name => "Tsering", :age => 25}
 end
 
 def pioneer
